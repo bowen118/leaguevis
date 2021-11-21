b0c842f8c913a129d3668d8e292307c3e03817ea
jkim1014 <joonyoungkim@college.harvard.edu>
2021-11-21 15:14:27 -0500

    Fix type for variables. Add colors range and color scale.

--- js/innovative.js
+++ js/innovative.js
@@ -2,6 +2,7 @@
 *    class innovative      *
 * * * * * * * * * * * * * */
 let normalizedColumns = ["team kpm", "dragons", "heralds", "barons", "towers", "inhibitors", "dpm", "vspm", "earned gpm", "cspm", "xpdiffat15"]
+let colors = ['#3cb44b', '#ffe119', '#4363d8', '#46f0f0', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#aaffc3', '#ffd8b1', '#ffffff']
 
 class Innovative {
     constructor(parentElement, normalizedTeamData) {
@@ -13,10 +14,16 @@ class Innovative {
     }
 
     initVis() {
+        let vis = this
+
+        //scales
+        vis.color = d3.scaleOrdinal().domain(normalizedColumns).range(colors)
+
         this.wrangleData()
     }
 
     wrangleData() {
+        let vis = this
         this.updateVis()
     }
 
--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let scatterPlotVis, innovativeVis;
 let promises = [
     d3.csv("data/players.csv"),
     d3.csv("data/teams.csv"),
-    d3.csv("data/normalized_team.csv")
+    d3.csv("data/normalized_teams.csv")
 ]
 
 Promise.all(promises).then(data => initMainPage(data)).catch(err => console.log(err))
