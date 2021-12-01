f844731ec060734116bfee57314484032434a21f
Bowen Chen <bowen118@hotmail.com>
2021-12-01 15:30:19 -0500

    Add KDE of map, need scales and legend and formatting

---
+++ js/kde.js
@@ -0,0 +1,125 @@
+class KDE {
+    constructor(parentElement, data) {
+        this.parentElement = parentElement;
+        this.data = data;
+        this.side = "both";
+        this.initVis();
+    }
+
+    initVis() {
+        let vis = this;
+
+        vis.margin = {top: 50, right: 100, bottom: 100, left: 50};
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+        vis.dim = Math.min(vis.width, vis.height);
+
+        vis.data.forEach(d => d.time = +d.time);
+
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append('g')
+            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
+
+        vis.svg.append("svg:image")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", vis.dim)
+            .attr("height", vis.dim)
+            .attr("xlink:href", "img/minimap.png");
+
+        vis.x = d3.scaleLinear()
+            .domain([0, 512])
+            .range([0, vis.dim]);
+        vis.y = d3.scaleLinear()
+            .domain([0, 512])
+            .range([0, vis.dim]);
+
+        let slider = document.getElementById('slider');
+        let sliderMin = 0;
+        let sliderMax = 1;
+        noUiSlider.create(slider, {
+            start: [sliderMin, sliderMax],
+            connect: true,
+            range: {
+                "min": sliderMin,
+                "max": sliderMax
+            },
+            step: 0.01,
+            behaviour: "tap-drag",
+            // tooltips: true,
+            // format: {
+            //     to: d => d,
+            //     from: d => parseInt(d)
+            // },
+            pips: {
+                mode: "range",
+                density: 5
+            }
+        });
+        slider.style.height = "18px";
+        slider.style.width = "400px";
+        vis.start = sliderMin;
+        vis.end = sliderMax;
+        slider.noUiSlider.on('update', function (values) {
+            vis.start = values[0];
+            vis.end = values[1];
+            vis.wrangleData(vis.side);
+        });
+
+        vis.wrangleData(vis.side);
+    }
+
+    wrangleData(side) {
+        let vis = this;
+
+        vis.side = side;
+        if (vis.side === "red") {
+            vis.useData = vis.data.filter(d => d.red_side === "True");
+        } else if (vis.side === "blue") {
+            vis.useData = vis.data.filter(d => d.red_side === "False");
+        } else {
+            vis.useData = vis.data;
+        }
+
+        vis.filteredData = vis.useData.filter(d => d.time >= vis.start && d.time <= vis.end)
+
+        vis.displayData = d3.contourDensity()
+            .x(function(d) { return vis.x(d.left); })
+            .y(function(d) { return vis.y(d.top); })
+            .size([vis.width, vis.height])
+            .bandwidth(15)
+            (vis.filteredData);
+
+        vis.updateVis();
+    }
+
+    updateVis() {
+        let vis = this;
+
+        vis.color = d3.scaleLinear()
+            .domain([0, d3.max(vis.displayData, d => d.value)])
+        if (vis.side === "red") {
+            vis.color.range(["white", "red"]);
+        } else if (vis.side === "blue") {
+            vis.color.range(["white", "blue"]);
+        } else {
+            vis.color.range(["white", "purple"])
+        }
+
+        vis.kde = vis.svg
+            .selectAll("path")
+            .data(vis.displayData);
+
+        vis.kde.enter().append("path")
+            .attr("class", "red-path")
+            .merge(vis.kde)
+            // .transition().duration(800)
+            .attr("d", d3.geoPath())
+            .attr("fill", function(d) { return vis.color(d.value); })
+            .attr("opacity", 0.1);
+
+        vis.kde.exit().remove();
+    }
+}
--- js/main.js
+++ js/main.js
@@ -3,25 +3,27 @@
 * * * * * * * * * * * * * */
 
 // init global variables & switches
-let regionBar, regionCount, scatterPlotVis, areaChartVis, lineChartVis, innovativeVis;
+let regionBar, regionCount, scatterPlotVis, areaChartVis, lineChartVis, kdeVis, innovativeVis;
 
 let promises = [
     d3.csv("data/players.csv"),
     d3.csv("data/teams.csv"),
     d3.csv("data/overall_worlds_msi.csv"),
     d3.csv("data/normalized_teams.csv"),
-    d3.csv("data/coords.csv")
+    d3.csv("data/coords.csv"),
+    d3.csv("data/kill_coords.csv")
 ]
 
 Promise.all(promises).then(data => initMainPage(data)).catch(err => console.log(err))
 
 function initMainPage(dataArray) {
-    const [playersData, teamsData, tournament_data, normalizedTeamsData, coords] = dataArray
+    const [playersData, teamsData, tournament_data, normalizedTeamsData, coords, killCoords] = dataArray
     regionBar = new RegionBarChart("region-bar", tournament_data)
     regionCount = new RegionCountChart("region-count")
-    // innovativeVis = new Innovative("start", teamsData, normalizedTeamsData, coords)
+    innovativeVis = new Innovative("start", teamsData, normalizedTeamsData, coords)
     areaChartVis = new AreaChart("page3", teamsData)
     lineChartVis = new LineChart("page4", teamsData)
+    kdeVis = new KDE("kde", killCoords)
     // scatterPlotVis = new ScatterPlot("explore", playersData, teamsData)
 }
 
@@ -32,4 +34,9 @@ function dataChange(isPlayer, isFilter = false) {
 function categoryChange() {
     let selectedCategory = $('#categorySelector').val();
     regionBar.wrangleData(selectedCategory);
+}
+
+function sideChange() {
+    let selectedSide = $('#sideSelector').val();
+    kdeVis.wrangleData(selectedSide);
 }
